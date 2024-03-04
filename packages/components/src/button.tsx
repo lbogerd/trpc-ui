fcbc70cee439975cae449da23ce5ed91e881b945
lbogerd <l.bogerd@protonmail.com>
2024-03-05 00:45:41 +0100

    Add Components package

---
+++ packages/components/src/button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps {
+	children: React.ReactNode;
+	className?: string;
+	appName: string;
+}
+
+export const Button = ({ children, className, appName }: ButtonProps) => {
+	return (
+		<button
+			className={className}
+			onClick={() => alert(`Hello from your ${appName} app!`)}
+		>
+			{children}
+		</button>
+	);
+};
