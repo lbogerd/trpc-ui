0ff7a6cff5fad2a408434418c98fe4b08c952f13
Lucas Bogerd <lucas@youngones.works>
2024-04-20 10:57:46 +0200

    First semi-working version of ZodSchema to ReactHookForm

--- packages/components/src/components/zod-form/zod-form-input.tsx
+++ packages/components/src/components/zod-form/zod-form-input.tsx
@@ -1,39 +1,43 @@
 import { ZodTypeAny } from "zod";
 import { Checkbox } from "../ui/checkbox";
 import { Input } from "../ui/input";
+import { InputHTMLAttributes } from "react";
 
 type ZodFormInputProps = {
 	schema?: ZodTypeAny;
 	typeName?: string;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
-export function ZodFormInput({ schema, typeName }: ZodFormInputProps) {
+export function ZodFormInput(props: ZodFormInputProps) {
+	const { schema, typeName } = props;
+
 	if (schema && typeName) {
 		throw new Error("Only one of 'schema' or 'typeName' should be provided");
 	}
 
-	const parsedTypeName = schema?._def.typeName || typeName;
+	const parsedTypeName = (schema?._def?.typeName || typeName) ?? "";
 
 	switch (parsedTypeName) {
 		case "ZodString":
 			return (
 				<>
-					<h1>{typeName}</h1>
-					<Input />
+					<h1>{typeName ?? ""}</h1>
+					<Input {...props} />
 				</>
 			);
 		case "ZodNumber":
 			return (
 				<>
-					<h1>{typeName}</h1>
-					<Input type="number" />
+					<h1>{typeName ?? ""}</h1>
+					<Input type="number" {...props} />
 				</>
 			);
 		case "ZodBoolean":
 			return (
 				<>
-					<h1>{typeName}</h1>
-					<Checkbox />
+					<h1>{typeName ?? ""}</h1>
+					{/* @ts-ignore */}
+					<Checkbox {...props} />
 				</>
 			);
 		default:
--- packages/components/src/components/zod-form/zod-form.tsx
+++ packages/components/src/components/zod-form/zod-form.tsx
@@ -1,30 +1,51 @@
-import { ZodObject, ZodType } from "zod";
+import { ZodObject, ZodSchema, ZodType, z } from "zod";
 import { Button } from "../ui/button";
 import { ZodFormInput } from "./zod-form-input";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export function ZodForm(schema: ZodType<ZodObject<any>>) {
-	const objectSchema: { [key: string]: any } = (
-		schema._def as ZodObject<any>
-	).shape();
+export function ZodForm<T extends z.ZodType<ZodObject<any>>>(schema: T) {
+	const form = useForm<z.infer<typeof schema>>({
+		resolver: zodResolver(schema),
+	});
+
+	function onSubmit(values: z.infer<typeof schema>) {
+		window.alert(JSON.stringify(values, null, 2));
+	}
+
+	const objectSchema = schema;
 
 	return (
-		<form
-			onSubmit={(e) => {
-				e.preventDefault();
-				window.alert(JSON.stringify(objectSchema, null, 2));
-			}}
-		>
-			{Object.keys(objectSchema).map((key) => {
-				const fieldSchema = objectSchema[key];
-				return (
-					<div key={key}>
-						<label htmlFor={key}>{key}</label>
-						<ZodFormInput typeName={fieldSchema._def.typeName} />
-					</div>
-				);
-			})}
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				{Object.keys(objectSchema._def.shape()).map((key) => {
+					const fieldSchema = (objectSchema._def.shape() as any)[
+						key
+					] as ZodType<any>;
 
-			<Button type="submit">Submit</Button>
-		</form>
+					console.log("field schema:");
+					console.log(fieldSchema);
+					console.log(key);
+
+					return (
+						<FormField
+							control={form.control}
+							name={key as any}
+							key={key}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>{key}</FormLabel>
+									<FormControl>
+										<ZodFormInput schema={fieldSchema} {...field} />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+					);
+				})}
+				<Button type="submit">Submit</Button>
+			</form>
+		</Form>
 	);
 }
